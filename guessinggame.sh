bc973fd3059cc81e674417dce7721cea62bd8f9e
Milind Mohapatra <mohapatra.milind@gmail.com>
2020-08-28 21:43:20 +0530

    Add check function

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,9 +1,28 @@
 #!/usr/bin/env bash
 #File guessinggame.sh
 
+function check {
+  if [[ $input -eq $count ]]
+  then
+    return 0
+  elif [[ $input -gt $count ]]
+  then
+    echo "You guessed too high"
+    echo "Try again"
+    return 1
+  else
+    echo "You guessed too low"
+    echo "Try again"
+    return 2
+  fi
+}
+
 echo "How many files are there in the current directory?"
 echo "Go on, make a guess!"
 
 read input
 count=$(ls -l | wc -l)
-echo $count
+echo  "$input | $count"
+check
+flag=$?
+echo $flag
